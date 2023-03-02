fd6aeb81966cfcedd5910a756c89588b362fa0e1
Nicholas Boss <NicholasBoss0914@gmail.com>
2023-03-02 08:36:28 -0700

    edited the formatting of the cwgameselects.sql file

--- files/cwgamesselects.sql
+++ files/cwgamesselects.sql
@@ -17,7 +17,8 @@ USE cwgames;
 -- Difficulty: Easy
 -- Practice
 --------------------------------------
--- Find the title, release date, and rating id of all games that either contain the string 'Zelda' or have a rating M
+-- Find the title, release date, and rating id of all games 
+-- that either contain the string 'Zelda' or have a rating M
 
 
 --------------------------------------
@@ -28,7 +29,8 @@ USE cwgames;
 -- Difficulty: Medium
 -- Practice
 --------------------------------------
--- Find and round 25% of each price plus 3 dollars with an alias of 'Twenty-five Percent + $3'. 
+-- Find and round 25% of each price plus 3 dollars to 2 decimal places 
+-- with an alias of 'Twenty-five Percent + $3'. 
 
 
 --------------------------------------
@@ -36,8 +38,10 @@ USE cwgames;
 -- Difficulty: Easy
 -- Practice
 --------------------------------------
--- Get all rows of the game table with a single column called 'Game Name - Date' Where the column is the name and a dash with a 
--- year after it, like this: 'Legend of Zelda: Breath of the Wild - 2017'. Sort by release date in descending order.
+-- Get all rows of the game table with a single column called 'Game Name - Date' 
+-- Where the column is the name and a dash with a year after it, like this: 
+-- 'Legend of Zelda: Breath of the Wild - 2017'. 
+-- Sort by release date in descending order.
 
 
 --------------------------------------
@@ -45,9 +49,12 @@ USE cwgames;
 -- Difficulty: Medium
 -- Example
 --------------------------------------
--- You and a friend want to play through all of your games, but you want to do it in a random order. You have a
--- great idea to use your database to assign a random number to each game and sort it in ascending order
--- to create this list. You just played through all of the Assassins Creed games though, so you want to leave those out. 
+-- You and a friend want to play through all of your games, 
+-- but you want to do it in a random order. 
+-- You have a great idea to use your database to assign a random number
+-- to each game and sort it in ascending order to create this list.
+-- You just played through all of the Assassins Creed games though, 
+-- so you want to leave those out. 
 -- To try to avoid ties, make the random number between 1 and 100.
 
 
@@ -57,7 +64,9 @@ USE cwgames;
 -- Example
 --------------------------------------
 -- How long have the games been around? 
--- Round the date to the nearest year, add 'years' to the end of the number given and give an alias to the column name
+-- Round the date to the nearest year, 
+-- add 'years' to the end of the number given 
+-- and give an alias to the column name
 -- Add release date at the end to double check our work
 
 
@@ -85,7 +94,8 @@ USE cwgames;
 -- Difficulty: Hard
 -- Example
 --------------------------------------
---  Find all game companies that have charged more than $50 for a game. What are the games and their prices?
+--  Find all game companies that have charged more than $50 for a game.
+--  What are the games and their prices?
 
 
 --------------------------------------
@@ -94,7 +104,9 @@ USE cwgames;
 -- Practice
 --------------------------------------
 -- What is the company that released The Legend of Zelda: Twilight Princess?
--- Provide the game's rating, the platform(s) it is on, its price (formatted with a $), and its release date formated like: January 4th, 2010
+-- Provide the game's rating, the platform(s) it is on, 
+-- its price (formatted with a $),
+-- and its release date formated like: January 4th, 2010
 -- Provide aliases for your column names.
 
 
@@ -103,9 +115,11 @@ USE cwgames;
 -- Difficulty: Hard
 -- Example
 --------------------------------------
--- What is the largest city in the country of Argentina? What is the population of that city? What percent of the population speak that language?
--- Provide the city name, population, and percentage of the population that speaks the language in that city.
+-- What is the largest city in the country of Argentina? 
+-- What is the population of that city? 
+-- What percent of the population speak that language?
+-- Provide the city name, population, 
+-- and percentage of the population that speaks the language in that city.
 -- Sort them by population from greatest to least.
 -- Format the population and concat '%' to the percentage.
 -- Limit to the top 3 results.
-

62830ca13329374f2e51a953d9b8ec5750b35b68
NicholasBoss <nicholasboss0914@gmail.com>
2023-11-07 07:42:40 -0700

    updating information on concept workshop pages and updated forward engineering code for the workshop

--- files/cwgames-wscript.sql
+++ files/cwgames-wscript.sql
@@ -38,14 +38,13 @@ CREATE TABLE IF NOT EXISTS `cwgames`.`game` (
   `release_date` DATE NULL,
   `rating_id` INT UNSIGNED NULL,
   PRIMARY KEY (`game_id`),
-  CONSTRAINT `fk_game_rating1`
+  INDEX `fk_game_rating1_idx` (`rating_id` ASC) VISIBLE,
+  CONSTRAINT `game_fk1`
     FOREIGN KEY (`rating_id`)
     REFERENCES `cwgames`.`rating` (`rating_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-CREATE INDEX `fk_game_rating1_idx` ON `cwgames`.`game` (`rating_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
@@ -123,35 +122,31 @@ CREATE TABLE IF NOT EXISTS `cwgames`.`game_platform` (
   `price_id` INT UNSIGNED NULL,
   `has_played` TINYINT NULL,
   PRIMARY KEY (`game_id`, `platform_id`, `owner_id`),
-  CONSTRAINT `fk_gameplatform_game1`
+  INDEX `fk_gameplatform_platform1_idx` (`platform_id` ASC) VISIBLE,
+  INDEX `fk_gameplatform_game1_idx` (`game_id` ASC) VISIBLE,
+  INDEX `fk_game_platform_price1_idx` (`price_id` ASC) VISIBLE,
+  INDEX `fk_game_platform_owner1_idx` (`owner_id` ASC) VISIBLE,
+  CONSTRAINT `game_platform_fk1`
     FOREIGN KEY (`game_id`)
     REFERENCES `cwgames`.`game` (`game_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_gameplatform_platform1`
+  CONSTRAINT `game_platform_fk2`
     FOREIGN KEY (`platform_id`)
     REFERENCES `cwgames`.`platform` (`platform_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_game_platform_price1`
+  CONSTRAINT `game_platform_fk3`
     FOREIGN KEY (`price_id`)
     REFERENCES `cwgames`.`price` (`price_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_game_platform_owner1`
+  CONSTRAINT `game_platform_fk4`
     FOREIGN KEY (`owner_id`)
     REFERENCES `cwgames`.`owner` (`owner_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-CREATE INDEX `fk_gameplatform_platform1_idx` ON `cwgames`.`game_platform` (`platform_id` ASC) VISIBLE;
-
-CREATE INDEX `fk_gameplatform_game1_idx` ON `cwgames`.`game_platform` (`game_id` ASC) VISIBLE;
-
-CREATE INDEX `fk_game_platform_price1_idx` ON `cwgames`.`game_platform` (`price_id` ASC) VISIBLE;
-
-CREATE INDEX `fk_game_platform_owner1_idx` ON `cwgames`.`game_platform` (`owner_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
@@ -163,21 +158,19 @@ CREATE TABLE IF NOT EXISTS `cwgames`.`game_genre` (
   `game_id` INT UNSIGNED NOT NULL,
   `genre_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`game_id`, `genre_id`),
-  CONSTRAINT `fk_gamegenre_game1`
+  INDEX `fk_gamegenre_genre1_idx` (`genre_id` ASC) VISIBLE,
+  INDEX `fk_gamegenre_game1_idx` (`game_id` ASC) VISIBLE,
+  CONSTRAINT `game_genre_fk1`
     FOREIGN KEY (`game_id`)
     REFERENCES `cwgames`.`game` (`game_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_gamegenre_genre1`
+  CONSTRAINT `game_genre_fk2`
     FOREIGN KEY (`genre_id`)
     REFERENCES `cwgames`.`genre` (`genre_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-CREATE INDEX `fk_gamegenre_genre1_idx` ON `cwgames`.`game_genre` (`genre_id` ASC) VISIBLE;
-
-CREATE INDEX `fk_gamegenre_game1_idx` ON `cwgames`.`game_genre` (`game_id` ASC) VISIBLE;
 
 
 -- -----------------------------------------------------
@@ -189,21 +182,19 @@ CREATE TABLE IF NOT EXISTS `cwgames`.`game_company` (
   `game_id` INT UNSIGNED NOT NULL,
   `company_id` INT UNSIGNED NOT NULL,
   PRIMARY KEY (`game_id`, `company_id`),
-  CONSTRAINT `fk_gamecompany_game1`
+  INDEX `fk_gamecompany_company1_idx` (`company_id` ASC) VISIBLE,
+  INDEX `fk_gamecompany_game1_idx` (`game_id` ASC) VISIBLE,
+  CONSTRAINT `game_company_fk1`
     FOREIGN KEY (`game_id`)
     REFERENCES `cwgames`.`game` (`game_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_gamecompany_company1`
+  CONSTRAINT `game_company_fk2`
     FOREIGN KEY (`company_id`)
     REFERENCES `cwgames`.`company` (`company_id`)
     ON DELETE CASCADE
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
-
-CREATE INDEX `fk_gamecompany_company1_idx` ON `cwgames`.`game_company` (`company_id` ASC) VISIBLE;
-
-CREATE INDEX `fk_gamecompany_game1_idx` ON `cwgames`.`game_company` (`game_id` ASC) VISIBLE;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
